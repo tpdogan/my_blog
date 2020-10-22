95b687e6b6d04766489d0ba5a5ae871347115f54
Tamer Dogan <pelintamerdogan@gmail.com>
2020-10-22 13:25:56 +0200

    Comment destruction is created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "articles#index"
   resources :articles do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   resources :authors, only: [:show]
 
